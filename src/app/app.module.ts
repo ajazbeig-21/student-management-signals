c815a3476b8b6d7cd57965141b4689c3597f4948
ajaz@mycariq.com <ajaz@mycariq.com>
2025-05-02 17:01:44 +0530

    Student form added with Validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StudentFormComponent } from './component/student-form/student-form.component';
 import { StudentTableComponent } from './component/student-table/student-table.component';
+import { MatSelectModule } from '@angular/material/select';
+import { ErrorStateMatcher, MatNativeDateModule, provideNativeDateAdapter, ShowOnDirtyErrorStateMatcher } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -14,9 +22,18 @@ import { StudentTableComponent } from './component/student-table/student-table.c
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatRadioModule,
+    MatCheckboxModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatButtonModule
   ],
-  providers: [],
+  providers: [provideNativeDateAdapter()],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/component/student-form/student-form.component.ts
+++ src/app/component/student-form/student-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-student-form',
@@ -7,5 +8,27 @@ import { Component } from '@angular/core';
   styleUrl: './student-form.component.css'
 })
 export class StudentFormComponent {
+  studentForm!: FormGroup;
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.studentForm = this.fb.group({
+      name: ['', Validators.required],
+      dob: ['', Validators.required],
+      gender: ['', Validators.required],
+      address: ['', [Validators.required, Validators.minLength(10)]],
+      email: ['', [Validators.required, Validators.email]],
+      mobile: ['', [Validators.required, Validators.pattern(/^[6-9]\d{9}$/)]],
+      terms: [false, Validators.requiredTrue]
+    });
+  }
+
+  onSubmit() {
+    if (this.studentForm.valid) {
+      console.log(this.studentForm.value);
+    } else {
+      this.studentForm.markAllAsTouched(); // Show validation errors
+    }
+  }
 }
