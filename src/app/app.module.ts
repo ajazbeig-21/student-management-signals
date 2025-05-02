b256403a5c005ef0a6c6666db0df17d015c01c16
ajaz@mycariq.com <ajaz@mycariq.com>
2025-05-02 16:44:37 +0530

    form and table component added | Angular Material Setup Done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StudentFormComponent } from './component/student-form/student-form.component';
+import { StudentTableComponent } from './component/student-table/student-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentFormComponent,
+    StudentTableComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/component/student-table/student-table.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-student-table',
+  standalone: false,
+  templateUrl: './student-table.component.html',
+  styleUrl: './student-table.component.css'
+})
+export class StudentTableComponent {
+
+}
